c216565d51ad9fcd9493d8806957b0d0f27b88db
thorped1 <d.thorpe3@lancaster.ac.uk>
2025-12-30 11:31:34 +0000

    Created tutorial 3

---
+++ Tutorial 3/models.py
@@ -0,0 +1,38 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import text
+
+
+db = SQLAlchemy()
+db_name = 'database.db'
+
+
+class Crop(db.Model):
+    __tablename__ = "Crops"
+
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    sowing = db.Column(db.String)
+    targetYield = db.Column(db.Float)
+
+    fields = db.relationship("Field", back_populates="crop_rel")
+
+
+class Field(db.Model):
+    __tablename__ = "Fields"
+
+    id = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    area = db.Column(db.Float)
+    soilType = db.Column(db.String)
+    risk = db.Column(db.String)
+    irrigation = db.Column(db.String)
+    drainage = db.Column(db.String)
+    pH = db.Column(db.Float)
+    SOM = db.Column(db.Float)
+
+    # Foreign key column (this is the key missing piece)
+    crop = db.Column(db.String, db.ForeignKey("Crops.id"), nullable=True)
+
+    # Relationship (this gives you field.crop)
+    crop_rel = db.relationship("Crop", back_populates="fields")
+
